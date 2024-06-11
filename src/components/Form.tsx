24d05612616ddd2a0e26b01bb52ea1c4c43e4d26
Ivan Gutierrez <ivan.gutierrez@techko.com.au>
2024-06-11 10:17:13 +1000

    calorie-tracker initial commit

---
+++ src/components/Form.tsx
@@ -0,0 +1,74 @@
+import { categories } from "../data/categories"
+import { useState } from "react"
+
+export default function Form() {
+
+  const[activity, setActivity] = useState({
+    category: 1,
+    name:'',
+    calories: 0
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setActivity({
+      ...activity,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  return (
+	<form
+		className="space-y-5 bg-white shadow p-10 rounded-lg"
+	>
+    <div className="grid grid-cols-1 gap-3">
+      <label htmlFor="category" className="font-bold">Categoría:</label>
+      <select
+        className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+        id="category"
+        value={activity.category}
+        onChange={handleChange}
+      >
+        {categories.map( category => (
+          <option
+            key={category.id}
+            value={category.id}
+          >
+            {category.name}
+          </option>
+        ))}
+      </select>
+    </div>
+
+    <div className="grid grid-cols-1 gap-3">
+      <label htmlFor="name" className="font-bold">Actividad:</label>
+      <input
+        id="name"
+        type="text"
+        className="border border-slate-300 p-2 rounded-lg"
+        placeholder="Ej. Comida, Jugo de naranja, Ensalada, Ejercicio, Pesas"
+        value={activity.name}
+        onChange={handleChange}
+      />
+    </div>
+
+    <div className="grid grid-cols-1 gap-3">
+      <label htmlFor="calories" className="font-bold">Calorias:</label>
+      <input
+        id="calories"
+        type="number"
+        className="border border-slate-300 p-2 rounded-lg"
+        placeholder="Calorias. Ej. 300 o 500"
+        value={activity.calories}
+        onChange={handleChange}
+      />
+    </div>
+
+    <input
+      type="submit"
+      className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+      value="Guardar comida o ejercicio"
+    />
+
+	</form>
+  )
+}
